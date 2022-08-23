64181f9af5389df5c15976b3a1be35925598d9fd
b3nam <51991614+b3nam@users.noreply.github.com>
2022-08-23 23:14:25 +0430

    Add files via upload

---
+++ b3nam_cryption.py
@@ -0,0 +1,21 @@
+﻿# A program from B3nam
+# Please Run this code on Python version 3 .x
+from tkinter import filedialog
+
+tf = filedialog.askopenfilename(
+        title="Open Text file", 
+        filetypes=(("Encrypted Files", "*"),)
+        )
+fw = filedialog.asksaveasfile(
+        title="Save to", 
+        filetypes=(("Decrypted Files", "*.txt"),)
+        )
+fr = open(tf, "rb")
+content = fr.read()
+decrypted = ""
+key = 0x5a
+for c in content:
+    decrypted += chr(int(c) ^ key)
+fw.write(decrypted)
+fw.close()
+fr.close()
