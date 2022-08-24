70cadf50cead38e09fb387a6f98e2a01dba6cada
b3nam <51991614+b3nam@users.noreply.github.com>
2022-08-24 23:28:37 +0430

    Update b3nam_tools.py

--- b3nam_tools.py
+++ b3nam_tools.py
@@ -2,6 +2,7 @@
 # !/usr/bin/python3
 from tkinter import *
 from tkinter import messagebox
+from tkinter import filedialog
 import requests
 
 root = Tk()
@@ -10,6 +11,27 @@ root.geometry("500x200")
 a = requests.get('https://api64.ipify.org/')
 def IP():
    messagebox.showinfo("My IP ", a.text)
+def cryption():
+   tf = filedialog.askopenfilename(
+        title="Open Text file", 
+        filetypes=(("Encrypted Files", "*"),)
+        )
+   fw = filedialog.asksaveasfile(
+        title="Save to", 
+        filetypes=(("Decrypted Files", "*.txt"),)
+        )
+   fr = open(tf, "rb")
+   content = fr.read()
+   decrypted = ""
+   key = 0x5a
+   for c in content:
+       decrypted += chr(int(c) ^ key)
+   fw.write(decrypted)
+   fw.close()
+   fr.close()
 
 B1 = Button(root, text = "What is My Public IP", command = IP)
 B1.place(x = 60,y = 50)
+
+B2 = Button(root, text = "Decrypt Files", command = cryption)
+B2.place(x = 300,y = 50)
