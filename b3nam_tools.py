86a025121ed14537019f9767c152ee368100ce18
b3nam <51991614+b3nam@users.noreply.github.com>
2022-08-24 16:11:51 +0430

    Add files via upload

---
+++ b3nam_tools.py
@@ -0,0 +1,8 @@
+﻿# A program from B3nam
+# Please Run this code on Python version 3 .x
+
+
+import requests
+
+a = requests.get('https://api64.ipify.org/')
+print("Public IPV4/6 address is :"," ",a.text)
